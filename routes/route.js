ffda443761d9bc7e20ca4984c2e4c664a48ee146
Pandax19 <ajwhitley@comcast.net>
2023-05-29 21:53:59 -0400

    defined variables and added if-block in routes.js

---
+++ routes/route.js
@@ -0,0 +1,34 @@
+const uuid = require("../helpers/utils");
+const express = require("express");
+const fs = require("fs");
+const router = express.Router();
+
+
+
+router.post('/notes', (req, res) => {
+
+    const { title, text } = req.body;
+
+    if ( title && text ) {
+        
+        const newNote = {
+            title : title,
+            text: text,
+            id : uuid(),
+        };
+
+        readAndAppend(newNote, '../db/db.json');
+
+        const response = {
+            status: 'success',
+            body: newNote,
+          };
+
+          res.json(response);
+        }else{
+            res.json('Error in posting note');
+        }
+    }
+)
+
+module.exports = router; 
